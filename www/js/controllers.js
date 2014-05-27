4f437efda393dc9137b5999194223f7a77c93205
Leonardo Borges <leonardoborges.rj@gmail.com>
2014-05-27 12:45:38 +1000

    Add absolute acceleration

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,16 +2,25 @@ function onError() {
   alert('onError!');
 };
 
+var GRAVITY= 9.8;
+
+function absAcceleration(x, y, z){
+  return Math.sqrt(x*x + y*y + z*z) - GRAVITY;
+}
+
+
 angular.module('starter.controllers', [])
 
   .controller('DashCtrl', function($scope) {
-    $scope.acceleration = "0,0,0";
+    $scope.acceleration = "0";
     var options = { frequency: 500 };
     var watchID = navigator.accelerometer.watchAcceleration(function(acceleration){
-      $scope.acceleration = "X: " + acceleration.x + " Y: " + acceleration.y + "Z: " + acceleration.z;
+      $scope.acceleration = absAcceleration(acceleration.x, acceleration.y, acceleration.z);
+      console.log( "AbsAccel: ->( " + $scope.acceleration +" )<-");
+
+// "X: " + acceleration.x + " Y: "
+//         + acceleration.y + "Z: " + acceleration.z;
       $scope.$apply();
-      console.log("Got acc: " + $scope.acceleration);
-      console.log("(X: " + acceleration.x + " Y: " + acceleration.y + "Z: " + acceleration.z + ")");
     }, onError, options);
   })
 

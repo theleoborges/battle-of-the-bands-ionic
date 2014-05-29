b69ddfab6b6b599b7123c31e5949aa2507bec1a5
Andrew Carr <andrew@2metres.com>
2014-05-29 12:06:27 +1000

    Add mixin to fade background based on data-aceel

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,43 @@
+function onError() {
+  alert('onError!');
+};
+
+var GRAVITY= 9.8;
+
+function absAcceleration(x, y, z){
+  return Math.sqrt(x*x + y*y + z*z) - GRAVITY;
+}
+
+
+var SERVER = "http://10.11.2.19:3000";
+//var SERVER = "http://192.168.1.102:3000";
+
+angular.module('starter.controllers', [])
+
+  .controller('DashCtrl', function($scope, $http) {
+    $scope.acceleration = "0";
+    var options = { frequency: 500 };
+    var watchID = navigator.accelerometer.watchAcceleration(function(acceleration){
+      $scope.acceleration = absAcceleration(acceleration.x, acceleration.y, acceleration.z);
+      console.log( "AbsAccel: ->( " + $scope.acceleration +" )<-");
+      $http.post(SERVER + '/accelerations', JSON.stringify({
+        accel: $scope.acceleration,
+        uuid: device.uuid
+      })).success(function(){
+        console.log( 'posted' );
+      });
+
+      $scope.$apply();
+    }, onError, options);
+  })
+
+  .controller('FriendsCtrl', function($scope, Friends) {
+    $scope.friends = Friends.all();
+  })
+
+  .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+    $scope.friend = Friends.get($stateParams.friendId);
+  })
+
+  .controller('AccountCtrl', function($scope) {
+  });
